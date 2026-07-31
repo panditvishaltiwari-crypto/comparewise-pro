0de280927c3bc1e7619da33e573a51688bdffe7b
panditvishaltiwari-crypto <panditvishaltiwari@gmail.com>
2026-08-01 01:13:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,57 @@
+/* =================================
+   CompareWise Configuration
+================================= */
 
+
+const CONFIG = {
+
+    brandName: "CompareWise",
+
+    websiteType: "Affiliate Comparison Platform",
+
+    offers: [
+
+        {
+            name: "Home Improvement",
+            url: "#"
+        },
+
+        {
+            name: "Finance Offers",
+            url: "#"
+        },
+
+        {
+            name: "Insurance",
+            url: "#"
+        },
+
+        {
+            name: "Business Services",
+            url: "#"
+        }
+
+    ],
+
+
+    contactEmail: "support@comparewise.com",
+
+
+    socialLinks: {
+
+        facebook: "#",
+        twitter: "#",
+        instagram: "#"
+
+    }
+
+};
+
+
+// Export
+
+if(typeof module !== "undefined"){
+
+    module.exports = CONFIG;
+
+}
